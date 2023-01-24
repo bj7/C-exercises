5dc246e17d94b846e2551280a96494d02d19397b
Josh Bernitt <jjbernitt@gmail.com>
2023-01-24 11:31:44 -0500

    tabs

--- reversePolishCalc.c
+++ reversePolishCalc.c
@@ -21,156 +21,156 @@ double pop(void);
 
 int main()
 {
-    int type;
-    double op2;
-    char s[MAXOP];
+	int type;
+	double op2;
+	char s[MAXOP];
 
-    while ((type = getop(s)) != EOF) {
-        switch(type) {
-            case NUMBER: 
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0) {
-                    push(pop() / op2);
-                } else {
-                    printf("error: zero divisor\n");
-                }
-                break;
-            case '%':
-                op2 = pop();
-                int result = (int)pop() % (int)op2;
-                push((double)result);
-                break;
-            case 'p': // print top 
-                printf("\t%.8g\n", getTop());
-                break;
-            case 'd': // duplicate top
-                op2 = getTop();
-                push(op2);
-                break;
-            case 's': // swap top two
-                op2 = pop();
-                double nt = pop();
-                push(op2);
-                push(nt);
-                break;
-            case 'c': // clear
-                sp = 0;
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
-        }
-    }
-    return 0;
+	while ((type = getop(s)) != EOF) {
+		switch(type) {
+			case NUMBER: 
+				push(atof(s));
+				break;
+			case '+':
+				push(pop() + pop());
+				break;
+			case '*':
+				push(pop() * pop());
+				break;
+			case '-':
+				op2 = pop();
+				push(pop() - op2);
+				break;
+			case '/':
+				op2 = pop();
+				if (op2 != 0.0) {
+					push(pop() / op2);
+				} else {
+					printf("error: zero divisor\n");
+				}
+				break;
+			case '%':
+				op2 = pop();
+				int result = (int)pop() % (int)op2;
+				push((double)result);
+				break;
+			case 'p': // print top 
+				printf("\t%.8g\n", getTop());
+				break;
+			case 'd': // duplicate top
+				op2 = getTop();
+				push(op2);
+				break;
+			case 's': // swap top two
+				op2 = pop();
+				double nt = pop();
+				push(op2);
+				push(nt);
+				break;
+			case 'c': // clear
+				sp = 0;
+				break;
+			case '\n':
+				printf("\t%.8g\n", pop());
+				break;
+			default:
+				printf("error: unknown command %s\n", s);
+				break;
+		}
+	}
+	return 0;
 }
 
 void push(double f) 
 {
-    if (sp < MAXVAL) {
-        // printf("double f %g\n", f);
-        val[sp++] = f;
-    } else {
-        printf("error: stack full, can't push %g\n", f);
-    }
+	if (sp < MAXVAL) {
+		// printf("double f %g\n", f);
+		val[sp++] = f;
+	} else {
+		printf("error: stack full, can't push %g\n", f);
+	}
 }
 
 double getTop(void)
 {
-    if (sp > 0) {
-        double r = val[sp-1];
-        // printf("val[--sp] %g\n", r);
-        return r; 
-    } else {
-        printf("error: stack empty\n");
-        return 0.0;
-    }
+	if (sp > 0) {
+		double r = val[sp-1];
+		// printf("val[--sp] %g\n", r);
+		return r; 
+	} else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
 }
 
 double pop(void)
 {
-    if (sp > 0) {
-        double r = val[--sp];
-        // printf("val[--sp] %g\n", r);
-        return r; 
-    } else {
-        printf("error: stack empty\n");
-        return 0.0;
-    }
+	if (sp > 0) {
+		double r = val[--sp];
+		// printf("val[--sp] %g\n", r);
+		return r; 
+	} else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
 }
 
 int getop(char s[])
 {
-    int i, c;
+	int i, c;
 
-    while((s[0] = c = getch()) == ' ' || c == '\t') {
-        continue;
-    }
+	while((s[0] = c = getch()) == ' ' || c == '\t') {
+		continue;
+	}
 
-    s[1] = '\0';
+	s[1] = '\0';
 
-    if (!isdigit(c) && c != '.' && c != '-') {
-        return c;
-    }
+	if (!isdigit(c) && c != '.' && c != '-') {
+		return c;
+	}
 
-    i = 0;
-    
-    if (isdigit(c)) {
-        // while (isdigit(s[++i] = c = getch())) {
-        //     continue;
-        // }
-        while (isdigit(c)) {
-            c = getch();
-            s[++i] = c;
-        }
-    }
+	i = 0;
+	
+	if (isdigit(c)) {
+		// while (isdigit(s[++i] = c = getch())) {
+		//     continue;
+		// }
+		while (isdigit(c)) {
+			c = getch();
+			s[++i] = c;
+		}
+	}
 
-    if (c == '.') {
-        while (isdigit(s[++i] = c = getch())) {
-            continue;
-        }
-    }
+	if (c == '.') {
+		while (isdigit(s[++i] = c = getch())) {
+			continue;
+		}
+	}
 
-    if (c == '-') {
-        while (isdigit(s[++i] = c = getch())) {
-            continue;
-        }
-    }
+	if (c == '-') {
+		while (isdigit(s[++i] = c = getch())) {
+			continue;
+		}
+	}
 
-    s[i] = '\0'; // EOF
+	s[i] = '\0'; // EOF
 
-    if (c != EOF) {
-        ungetch(c);
-    }
+	if (c != EOF) {
+		ungetch(c);
+	}
 
-    return NUMBER;
+	return NUMBER;
 }
 
 int getch(void)
 {
-    return (bufp > 0) ? buf[--bufp] : getchar();
+	return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
 void ungetch(int c)
 {
-    if (bufp >= BUFSIZE) {
-        printf("ungetch: too many characters\n");
-    } else {
-        buf[bufp++] = c;
-    }
+	if (bufp >= BUFSIZE) {
+		printf("ungetch: too many characters\n");
+	} else {
+		buf[bufp++] = c;
+	}
 }
